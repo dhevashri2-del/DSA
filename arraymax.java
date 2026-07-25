86298fdaa86c2dbde4074bce372354d42db64b6a
dhevashri2-del <dhevashri2@gmail.com>
2026-07-25 22:13:03 +0530

    Add files via upload

---
+++ arraymax.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class arraymax {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int[] arr = new int[n];
+       int max = arr[0];
+       for (int i = 0; i < n; i++) {
+             arr[i] = sc.nextInt();
+             if(max < arr[i]){
+                 max = arr[i];
+            }
+}
+        System.out.println("Max : " + max);
+
+            }    
+        }
